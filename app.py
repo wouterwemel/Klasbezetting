3dede71992d050c0710c83045e487cfd9410c166
wouterwemel <145281993+wouterwemel@users.noreply.github.com>
2025-10-18 15:54:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ title = "Klasbezetting TEST TOOL"
 
 jaar = sidebar.multiselect(
   label = "jaar",
-  options = df["Jaar"].sort_values().unique()
+  options = df["Jaar"].sort_values()
 
 lkr = sidebar.multiselect(
   label = "Leerkracht(en)",
