68919bcd7d337636b281c249e1c6da7fb3d34064
wouterwemel <145281993+wouterwemel@users.noreply.github.com>
2025-10-18 14:49:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 
 df_excel = read_excel("Klasbzetting KADE - 2025-2026.xlsx")
 df_json = df_excel.to_json()
+print(df_json)
 
 set_page_config(page_title="Klasbezetting KADE",
                 page_icon=":bar_chart:",
