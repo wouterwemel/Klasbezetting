8725959acb0584cbe81678482f5c8d0c10d24300
wouterwemel <145281993+wouterwemel@users.noreply.github.com>
2025-10-20 09:24:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ else:
     label = "Datum",
     format = "DD/MM/YYYY",
     value = "today")
-jaar = datum["YYYY]
+jaar = datum["YYYY"]
 
 alle_weekdagen = sidebar.toggle(label = "Alle weekdagen", value = True)
 if alle_weekdagen:
