b49a65e1c89e1845e4b94117f19690bb0592b4b7
wouterwemel <145281993+wouterwemel@users.noreply.github.com>
2025-10-21 12:54:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if alle_data:
 else:
   datum = date_input(
     label = "Datum",
-    format = "DD/MM/YYYY",
+    format = "D/MM/YYYY",
     value = "today")
 
 alle_weekdagen = sidebar.toggle(label = "Alle weekdagen", value = True)
